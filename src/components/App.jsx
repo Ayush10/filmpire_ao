e255c3166ddea630fcefdc322f4c61ee9e68b915
Ayush10 <ayushgreat10@gmail.com>
2022-11-28 13:47:57 +0545

    Directory Setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { CssBaseline } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+
+import useStyles from "./styles";
+
+import { Actors, MovieInformation, Movies, NavBar, Profile } from "./";
 
 const App = () => {
-  return <div>App</div>;
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className="classes.toolbar" />
+        <Routes>
+          <Route exact path="/movies:id" element={<MovieInformation />} />
+          <Route exact path="/actors/" element={<Actors />} />
+          {/* <Route exact path="/actors:id" element={"Actor List"} /> */}
+          <Route exact path="/" element={<Movies />} />
+          <Route exact path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </main>
+    </div>
+  );
 };
 
 export default App;
